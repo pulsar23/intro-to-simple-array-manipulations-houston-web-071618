73a25e9d4a2f88cbdcdaef5137fc4c0b8524ae19
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-07 23:17:06 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,4 +11,16 @@ def using_pop(array)
 end
 
 def pop_with_args
+  return array.pop(2)
+end
+
+def using_shift(array)
+  return array.shift 
+end
+
+def shift_with_args(array)
+  return array.shift(2)
+end
+
+def using_concat(array1, array2)
   
